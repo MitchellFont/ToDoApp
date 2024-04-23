8d3fbd4d393404b40bf4643acda162d75b240f0e
M3esh <105067754+M3esh@users.noreply.github.com>
2024-04-23 13:11:39 -0500

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return a list of to-do items."""
+    with open(filepath, 'r') as local_file:
+        local_todos = local_file.readlines()
+    return local_todos
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write to-do items to a list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello!")
+    print(get_todos())
