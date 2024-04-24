25f8e0ecbcb18d86d28aeb8bd161adc76a33374c
M3esh <105067754+M3esh@users.noreply.github.com>
2024-04-24 15:43:23 -0500

    Finish Web App

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ st.title("My To-Do App")
 st.subheader("This is my To-Do App")
 st.write("This app is used to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Enter a To-Do",
               on_change=add_todo, key='new_todo')
